72c75da362337fe530647b0faeea02cf9ef76013
varan <bjiad22@gmail.com>
2026-07-14 22:57:48 +0300

    add files

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -7,45 +7,29 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TextBoxTests extends TestBase {
 
     @Test
     void succesfulFillRormTests() {
+        open("/automation-practice-form");
+        $("#firstName").setValue("Roman");
+        $("#lastName").setValue("Ivanov");
+        $("#userEmail").setValue("roman@ivi.ru");
+        $("#userNumber").setValue("8999655666");
+        $("#gender-radio-1").click();
 
-        open("https://demoqa.com/automation-practice-form");
-        $("[id=firstName]").setValue("Roman");
-        $("[id=lastName]").setValue("Ivanov");
-        $("[id=userEmail]").setValue("roman@ivi.ru");
-        $("[id=userNumber]").setValue("8999655666");
-        $("[for=gender-radio-1]").click();
-        $("[id=react-select-2-live-region]").setValue("Hindi");
-        $("[id=hobbies-checkbox-1]").click();
-        $("[id=hobbies-checkbox-3").click();
-        $(".css-hlgwow").click();
-        $("[id=submit]").click();
+        $("#subjectsInput").setValue("E");
+        $$("[role='option']").get(0).shouldBe(visible).click();
+        $("#subjectsInput").clear();
+        $("#subjectsInput").setValue("H");
+        $$("[role='option']").get(0).shouldBe(visible).click();
+        $("#hobbies-checkbox-3").click();
+        $("#uploadPicture").click();
+        $("#currentAddress").setValue("Москва, проспект Андропова");
 
-        $(".modal-content").shouldBe(visible);
-      //  $("tbody tr:first-child td:last-child").shouldHave(text("Roman Ivanov"));
-
-    }
-    @Test
-    void succesfulFillRormTests() {
-        Configuration.browserSize = "1920x1080";
-
-        open("https://demoqa.com/automation-practice-form");
-        $("[id=firstName]").setValue("Roman");
-        $("[id=lastName]").setValue("Ivanov");
-        $("[id=userEmail]").setValue("roman@ivi.ru");
-        $("[id=userNumber]").setValue("8999655666");
-        $("[for=gender-radio-1]").click();
-        $("[id=react-select-2-live-region]").setValue("Hindi");
-        $("[id=hobbies-checkbox-1]").click();
-        $("[id=hobbies-checkbox-3").click();
-        $(".css-hlgwow").click();
-        $("[id=submit]").click();
+        $("#submit").click();
 
         $(".modal-content").shouldBe(visible);
         //  $("tbody tr:first-child td:last-child").shouldHave(text("Roman Ivanov"));
